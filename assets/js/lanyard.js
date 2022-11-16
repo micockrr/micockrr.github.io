aa8f66ba959ad5f03119326b022b186ac2d65135
micockrr <77680260+micockrr@users.noreply.github.com>
2022-11-16 17:37:34 +0100

    orkeuas
    dzialaj

---
+++ assets/js/lanyard.js
@@ -0,0 +1,100 @@
+API_URL = 'https://api.lanyard.rest/v1';
+USERID = '541284990281842710';
+
+// https://api.lanyard.rest/v1/users/374220001743208459
+
+async function fetchResponse(userId) {
+    try {
+        const url = await fetch(`${API_URL}/users/${userId}`);
+        const response = await url.json();
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+
+async function setAvatar() {
+    const response = await fetchResponse(USERID);
+
+    var avatarId = response.data.discord_user.avatar;
+    var fullUrl = `https://cdn.discordapp.com/avatars/${USERID}/${avatarId}`;
+
+    document.getElementById('pfp').src = fullUrl;
+}
+
+async function setAvatarFrame() {
+    const response = await fetchResponse(USERID);
+    const activity2 = document.getElementById('status2');
+
+    if (response.data.discord_status == 'offline') {
+        activity2.innerHTML = "Jestem offline";
+        activity2.style.cssText = 'color: unset; opacity: 0.5;';
+    } else {
+        activity2.innerHTML = "Jestem online";
+        activity2.style.cssText = 'color: #3ba45d; opacity: 1;';
+    }
+    switch (response.data.discord_status) {
+        case 'online':
+            document.getElementById('activity-dot').style.background =
+                '#3ba45d';
+            document.getElementById('activity-dot').title = "Online";
+            activity2.innerHTML = "Jestem online";
+            break;
+
+        case 'dnd':
+            document.getElementById('activity-dot').style.background =
+                '#ed4245';
+            document.getElementById('activity-dot').title = 'Do not disturb';
+            break;
+
+        case 'idle':
+            document.getElementById('activity-dot').style.background =
+                '#faa81a';
+            document.getElementById('activity-dot').title = 'Idle';
+            break;
+
+        case 'offline':
+            document.getElementById('activity-dot').style.background =
+                '#747e8c';
+            document.getElementById('activity-dot').title = 'Offline';
+            break;
+    }
+}
+
+async function setUsername() {
+    const response = await fetchResponse(USERID);
+
+    var user = response.data.discord_user.username;
+    var discriminator = response.data.discord_user.discriminator;
+    var fullName = `${user}#${discriminator}`;
+
+    document.getElementById('username').innerHTML = fullName;
+}
+
+async function setStatus() {
+    const response = await fetchResponse(USERID);
+
+    if (response.data.discord_status == 'offline') {
+        return;
+    }
+    var status = response.data.activities[0].state;
+    if (!status) {
+        return;
+    }
+    document.getElementById('status').innerHTML = `Status: "${status}"`;
+}
+
+async function statusInvoke() {
+    setStatus();
+    setAvatarFrame();
+}
+
+async function invoke() {
+    setAvatar();
+    setUsername();
+    setInterval(statusInvoke, 1000);
+}
+
+invoke();
